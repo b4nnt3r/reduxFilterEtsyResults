dc7cad7eb10904f64a16f555cca7ea9a70becab2
Brandon Walden <brandonshanewalden@gmail.com>
2017-09-25 09:50:45 -0400

    starter files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import 'shoelace-css/dist/shoelace.css';
 import './App.css';
 
+import FilterProducts from "./containers/FilterProducts";
+import ProductList from "./containers/ProductList";
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h2>Show "Belt" related items</h2>
+                <FilterProducts/>
+                <ProductList/>
+            </div>
+        );
+    }
 }
 
 export default App;
